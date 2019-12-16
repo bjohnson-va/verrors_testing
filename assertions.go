5c86a1be9213c9904515e92fa4c9a7d64293f50d
[Scrummibears] Brad Johnson <bjohnson@vendasta.com>
2019-12-16 14:05:10 -0600

    Added assertion for matching types between errors

--- assertions.go
+++ assertions.go
@@ -17,6 +17,23 @@ func AssertErrorTypeMatches(t *testing.T, expectedType verrors.ErrorType, actual
 	}
 }
 
+func AssertErrorTypesMatch(t *testing.T, expectedErr error, actualErr error) {
+	if actualErr == nil && expectedErr != nil {
+		t.Errorf("Expected %s error but got nil", expectedErr.Error())
+		return
+	}
+	if actualErr != nil && expectedErr == nil {
+		t.Errorf("Expected nil error but got %s", actualErr.Error())
+		return
+	}
+	verr := verrors.FromError(actualErr)
+	ex := verrors.FromError(expectedErr)
+	if verr.ErrorType() != ex.ErrorType() {
+		t.Errorf("Expected %s but got %s (%s)", ex.ErrorType().String(), verr.ErrorType().String(), verr.Error())
+		return
+	}
+}
+
 func AssertVErrorIsNil(t *testing.T, actualErr error) {
 	if actualErr == nil {
 		return
