2e4c39d49eea8c09c5f88d36edae47b95423ba0a
(Email Team) Brad Johnson <bjohnson@vendasta.com>
2023-03-13 16:57:14 -0600

    feat(): Sow internal msg in more assertions + fuzz testing

--- assertions.go
+++ assertions.go
@@ -8,7 +8,11 @@ import (
 	"github.com/vendasta/gosdks/verrors"
 )
 
-func AssertErrorTypeEqual(t *testing.T, expectedType verrors.ErrorType, actualErr error) bool {
+func AssertErrorTypeEqual(
+	t *testing.T,
+	expectedType verrors.ErrorType,
+	actualErr error,
+) bool {
 	t.Helper()
 	if actualErr == nil {
 		t.Errorf("Expected %s error but got nil", expectedType.String())
@@ -16,13 +20,23 @@ func AssertErrorTypeEqual(t *testing.T, expectedType verrors.ErrorType, actualEr
 	}
 	verr := verrors.FromError(actualErr)
 	if verr.ErrorType() != expectedType {
-		t.Errorf("Expected %s but got %s (%s)", expectedType.String(), verr.ErrorType().String(), verr.Error())
+		t.Errorf(
+			"Expected %s but got %s (%s) []",
+			expectedType.String(),
+			verr.ErrorType().String(),
+			verr.Error(),
+			verr.GetInternalMessage(),
+		)
 		return false
 	}
 	return true
 }
 
-func AssertErrorTypesMatch(t *testing.T, expectedErr error, actualErr error) bool {
+func AssertErrorTypesMatch(
+	t *testing.T,
+	expectedErr error,
+	actualErr error,
+) bool {
 	t.Helper()
 	if actualErr == nil && expectedErr == nil {
 		return false
@@ -40,16 +54,19 @@ func AssertErrorTypesMatch(t *testing.T, expectedErr error, actualErr error) boo
 	if verr.ErrorType() != ex.ErrorType() {
 		t.Errorf(
 			"\n%10s: %20s (%s)\n"+
-				"%10s: %20s (%s)",
+				"%10s: %20s (%s) [%s]",
 			"Expected", ex.ErrorType().String(), ex.Error(),
-			"Actual", verr.ErrorType().String(), verr.Error(),
+			"Actual", verr.ErrorType().String(), verr.Error(), verr.GetInternalMessage(),
 		)
 		return false
 	}
 	return true
 }
 
-func AssertVErrorIsNil(t *testing.T, actualErr error) bool {
+func AssertVErrorIsNil(
+	t *testing.T,
+	actualErr error,
+) bool {
 	t.Helper()
 	if actualErr == nil {
 		return true
@@ -59,7 +76,9 @@ func AssertVErrorIsNil(t *testing.T, actualErr error) bool {
 		t, fmt.Sprintf(
 			`Expected nil but got:
 {type: %s, msg: "%s", internalMsg: "%s"}`,
-			verr.ErrorType().String(), verr.Error(), verr.GetInternalMessage(),
+			verr.ErrorType().String(),
+			verr.Error(),
+			verr.GetInternalMessage(),
 		),
 	)
 	return false
--- fuzz.go
+++ fuzz.go
@@ -1,9 +1,11 @@
 package verrors_testing
 
 import (
-	"github.com/vendasta/gosdks/verrors"
 	"math/rand"
+	"testing"
 	"time"
+
+	"github.com/vendasta/gosdks/verrors"
 )
 
 func RandomVError() verrors.ServiceError {
@@ -14,15 +16,93 @@ func RandomVError() verrors.ServiceError {
 }
 
 func RandomVErrorExcept(not ...verrors.ErrorType) verrors.ServiceError {
-	Choose:
-		for {
-			// TODO: There's almost certainly a way to do this that doesn't involve an infinite loop of retries
-			err := RandomVError()
-			for _, e := range not {
-				if err.ErrorType() == e {
-					continue Choose
-				}
+Choose:
+	for {
+		// TODO: There's almost certainly a way to do this that doesn't involve an infinite loop of retries
+		err := RandomVError()
+		for _, e := range not {
+			if err.ErrorType() == e {
+				continue Choose
 			}
-			return err
 		}
+		return err
+	}
+}
+
+func FuzzRandomVError(
+	f *testing.F,
+	testFn func(
+		t *testing.T,
+		vErrorCase verrors.ServiceError,
+	),
+) {
+	errors := []verrors.ErrorType{
+		verrors.NotFound,
+		verrors.InvalidArgument,
+		verrors.AlreadyExists,
+		verrors.PermissionDenied,
+		verrors.Unauthenticated,
+		verrors.Unimplemented,
+		verrors.Unknown,
+		verrors.Internal,
+		verrors.Gone,
+		verrors.Unavailable,
+		verrors.FailedPrecondition,
+		verrors.DeadlineExceeded,
+		verrors.ResourceExhausted,
+		verrors.Aborted,
+		verrors.Canceled,
+		verrors.BadGateway,
+	}
+	for _, vr := range errors {
+		f.Add(int(vr))
+	}
+	f.Fuzz(func(
+		t *testing.T,
+		errCode int,
+	) {
+		err := verrors.New(verrors.ErrorType(errCode), "error defined in fuzz test setup [verrors_testing]")
+		testFn(t, err)
+	})
+}
+
+func FuzzRandomVErrorExcept(
+	f *testing.F,
+	testFn func(
+		t *testing.T,
+		vErrorCase verrors.ServiceError,
+	),
+	except verrors.ErrorType,
+) {
+	errors := []verrors.ErrorType{
+		verrors.NotFound,
+		verrors.InvalidArgument,
+		verrors.AlreadyExists,
+		verrors.PermissionDenied,
+		verrors.Unauthenticated,
+		verrors.Unimplemented,
+		verrors.Unknown,
+		verrors.Internal,
+		verrors.Gone,
+		verrors.Unavailable,
+		verrors.FailedPrecondition,
+		verrors.DeadlineExceeded,
+		verrors.ResourceExhausted,
+		verrors.Aborted,
+		verrors.Canceled,
+		verrors.BadGateway,
+	}
+	for _, vr := range errors {
+		if vr == except {
+			continue
+		}
+		f.Add(int(vr))
+	}
+	f.Fuzz(func(
+		t *testing.T,
+		errCode int,
+	) {
+		err := verrors.New(verrors.ErrorType(errCode), "error defined in fuzz test setup [verrors_testing]")
+		testFn(t, err)
+	})
 }
