9266837fa1a11b44f8cbbb813e44c0a90dd79b45
(Email Team) Brad Johnson <bjohnson@vendasta.com>
2023-03-27 17:10:50 -0600

    fix(): Dead references

--- v2/assert/errors.go
+++ v2/assert/errors.go
@@ -1,23 +1,73 @@
 package verrors_assert
 
 import (
+	"fmt"
 	"testing"
-
-	"github.com/bjohnson-va/verrors_testing"
+	"github.com/bjohnson-va/verrors_testing/v2"
 	"github.com/vendasta/gosdks/verrors"
 )
 
 func ErrorTypeEqual(t *testing.T, expectedType verrors.ErrorType, actualErr error) bool {
 	t.Helper()
-	return verrors_testing.AssertErrorTypeEqual(t, expectedType, actualErr)
+	if actualErr == nil {
+		t.Errorf("Expected %s error but got nil", expectedType.String())
+		return false
+	}
+	verr := verrors.FromError(actualErr)
+	if verr.ErrorType() != expectedType {
+		t.Errorf(
+			"Expected %s but got %s (%s) []",
+			expectedType.String(),
+			verr.ErrorType().String(),
+			verr.Error(),
+			verr.GetInternalMessage(),
+		)
+		return false
+	}
+	return true
 }
 
 func ErrorTypesMatch(t *testing.T, expectedErr error, actualErr error) bool {
 	t.Helper()
-	return verrors_testing.AssertErrorTypesMatch(t, expectedErr, actualErr)
+	if actualErr == nil && expectedErr == nil {
+		return false
+	}
+	if actualErr == nil && expectedErr != nil {
+		t.Errorf("Expected %s error but got nil", expectedErr.Error())
+		return false
+	}
+	if actualErr != nil && expectedErr == nil {
+		t.Errorf("Expected nil error but got %s", actualErr.Error())
+		return false
+	}
+	verr := verrors.FromError(actualErr)
+	ex := verrors.FromError(expectedErr)
+	if verr.ErrorType() != ex.ErrorType() {
+		t.Errorf(
+			"\n%10s: %20s (%s)\n"+
+				"%10s: %20s (%s) [%s]",
+			"Expected", ex.ErrorType().String(), ex.Error(),
+			"Actual", verr.ErrorType().String(), verr.Error(), verr.GetInternalMessage(),
+		)
+		return false
+	}
+	return true
 }
 
 func NoError(t *testing.T, actualErr error) bool {
 	t.Helper()
-	return verrors_testing.AssertVErrorIsNil(t, actualErr)
+	if actualErr == nil {
+		return true
+	}
+	verr := verrors.FromError(actualErr)
+	verrors_testing.Fail(
+		t, fmt.Sprintf(
+			`Expected nil but got:
+{type: %s, msg: "%s", internalMsg: "%s"}`,
+			verr.ErrorType().String(),
+			verr.Error(),
+			verr.GetInternalMessage(),
+		),
+	)
+	return false
 }
--- v2/go.mod
+++ v2/go.mod
@@ -2,4 +2,17 @@ module github.com/bjohnson-va/verrors_testing/v2
 
 go 1.20
 
-require github.com/vendasta/gosdks/verrors v1.5.0
+require (
+	github.com/bjohnson-va/verrors_testing v1.1.4
+	github.com/vendasta/gosdks/verrors v1.5.0
+)
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	golang.org/x/sys v0.0.0-20200803210538-64077c9b5642 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200804131852-c06518451d9c // indirect
+	google.golang.org/grpc v1.31.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
